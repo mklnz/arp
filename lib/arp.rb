7c900be58b0bca2c22b787424b25cdf87dc96cb7
Michael Shi <michael@shi.co.nz>
2015-10-25 04:27:18 +0800

    Use arp -a (BSD style) for both Linux / OS X support

--- lib/arp.rb
+++ lib/arp.rb
@@ -8,27 +8,25 @@ module Arp
 
     extend Forwardable
     def_delegators :collection, :each, :<<, :size
-    
 
-    attr_reader :path
 
-    def initialize(path = "/proc/net/arp")
-      raise ArgumentError, "No such file: #{path}" unless File.exists?(path)
-      @path = path
+    attr_reader :cmd
+
+    def initialize(cmd = "arp -a")
+      @cmd = cmd
     end
 
     def collection
-      @collection ||= File.open(@path) do |f|
-        f.readlines[1..-1].map do |line|
-          columns = line.split(/\s+/)
-          ip, hw_type, flags, hw_address, mask, device = *columns
-          OpenStruct.new(ip: ip,
-                         hw_type: hw_type,
-                         flags: flags,
-                         hw_address: hw_address,
-                         mask: mask,
-                         device: device)
-        end
+      output = `#{@cmd}`.split("\n")
+
+      output.map do |line|
+        columns = line.split(/\s+/)
+        hostname = columns[0]
+        ip = columns[1][1..-2]
+        mac = columns[3]
+        OpenStruct.new(hostname: hostname,
+                       ip: ip,
+                       mac: mac)
       end
     end
   end
